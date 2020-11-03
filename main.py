1b36b1278a077c9fccbe608e531ae14816699086
Achew <artem.kovzik@yandex.ru>
2020-11-03 18:24:38 +0300

    Выполнил 1ое практическое задание

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ print(a)
 print(b)
 print(c)
 print(d)
+
+print('Введите любое число:')
+a1 = input()
+print('Введите любую строку')
+b1 = input()
+
+print('Вы ввели число: ' + a1)
+print('Вы ввели строку: ' + b1)
